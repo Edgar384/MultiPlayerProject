fabc22c7edf708bb5ebb930a12096d46c17d832a
Liad Halotzi <liadhalotzi@gmail.com>
2023-07-23 22:13:37 +0300

    Adding tank

--- Assets/TimeUI.cs
+++ Assets/TimeUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using GamePlayLogic;
 using TMPro;
 using UnityEngine;
@@ -7,10 +5,30 @@ using UnityEngine;
 public class TimeUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
+    private bool _is30SecondsReached;
+    private bool _is5SecondsReached;
+
+    private void Awake()
+    {
+        _is30SecondsReached = false;
+        _is5SecondsReached = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         _text.text = $"{TimeManager.TimeGame:0.00}s";
+
+        if (TimeManager.TimeGame < 0.3 && !_is30SecondsReached)
+        {
+            _is30SecondsReached = true;
+            GameplayAudioHandler.Instace.Play30SecondsLeft();
+        }
+
+        else if (TimeManager.TimeGame < 0.05 && !_is5SecondsReached)
+        {
+            _is5SecondsReached = true;
+            GameplayAudioHandler.Instace.Play5SecondsLeft();
+        }
     }
 }
